51af63c8ff7af1b583990c8364dc5fa1d800d6fe
zweicoder <zweicoder@gmail.com>
2017-02-24 16:21:49 +0800

    Update resume to use Elegant theme

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Web version to be hosted on GH-pages
 DATE=$(date +%Y-%m-%d)
-resume export index.html -t slick
+resume export index.html -t elegant
 
 # Shorter version for pdf
 resume export ./out/resume-pdf-$DATE.pdf -t onepage
